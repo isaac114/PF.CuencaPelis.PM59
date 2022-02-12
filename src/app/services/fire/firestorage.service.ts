da8dc82c1bc4dbdae2c16ca5ca3c4a200996410d
WilmerDurazno <34029478+WilmerDurazno@users.noreply.github.com>
2022-02-12 01:01:12 -0500

    seleccion de la imagen
    en el servidor firestore se creo una funcion para poder elegir una imagen de explorador de archivos par poner una imagende fondo

--- src/app/services/fire/firestorage.service.ts
+++ src/app/services/fire/firestorage.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FirestorageService {
 
-  constructor() { }
+  constructor(public fire: AngularFirestore) { }
+
+  guardarIma(){
+    return new Promise( resolve =>{
+      setTimeout(() => {
+        resolve(true);
+        console.log('responde a la promesa');
+        return;
+      }, 2000);
+    });
+  }
 }
