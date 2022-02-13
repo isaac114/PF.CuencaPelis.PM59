c031432e285585d9cd93cabb9b5524c46b8e1fbb
WilmerDurazno <34029478+WilmerDurazno@users.noreply.github.com>
2022-02-13 14:18:24 -0500

    directorio del principal
    al ejecutarse se muestra la pantalla principal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'pages/login',
+    redirectTo: 'principal',
     pathMatch: 'full'
   },
   {
